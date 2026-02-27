aeae07e16443666babe42565abc9b1bfd731c1f7
Alex Lankio <alex.lankio@proton.me>
2026-02-27 18:23:57 +0200

    Adds new link on SitesContainer.jsx

--- src/assets/SitesContainer.jsx
+++ src/assets/SitesContainer.jsx
@@ -1,10 +1,11 @@
 import isoheikkila from "./images/isoheikkila.png"
 import pokedex from "./images/pokedex.png"
+import reflections from "./images/reflections.png"
 import InfoCard from "./InfoCard"
 export default function SitesContainer()
 {
     return <div className="row justify-content-center my-3">
-            <h3 className="p-2 text-light"> Sites</h3>
+            <h3 className="p-2 text-light"> Sites and apps</h3>
             <InfoCard 
               name="Iso-Heikkilä's Clubroom Booking" 
               subtitle="React, css, JSON"
@@ -19,11 +20,21 @@ export default function SitesContainer()
               name="Pokédex" 
               subtitle="React & Bootstrap 5"
               picture={pokedex} 
-              alt=""
-              description="A small simple Pokédex site, which uses pokeapi to fetch information about the pokémon. "
+              alt="Photo of the website"
+              description="A small simple Pokédex site, which uses pokeapi to fetch information about the pokémon."
               linkAddress="https://mimirthful.github.io/bootstrap-react-pokedex/"
               linkName="Check out"
               />
+
+            <InfoCard 
+              name="Reflections" 
+              subtitle="React, MUI & express, SQL"
+              picture={reflections} 
+              alt="Photo of the app"
+              description="Ongoing project. It aims to be stoic journaling app using technologies common in fullstack."
+              linkAddress="https://github.com/mimirthful/reflections-journal/"
+              linkName="Check out"
+              />  
     
         </div>
 }
